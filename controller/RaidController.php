f9d766c4950ac5c7808bf201d388cffa05e9b18e
Jan Lička <j.licka.st@spseiostrava.cz>
2021-01-19 18:47:31 +0100

    správné zobrazení
    správné zobrazení je nyní funkční, předtím to vypisovalo více než mělo

--- controller/RaidController.php
+++ controller/RaidController.php
@@ -3,6 +3,11 @@ class RaidController extends Controller
 {
     public function process($parameters)
     {
+        $rname = text::camelCase($parameters[0]);
+        $sql = "SELECT b.idBosses, b.nameBoss, b.difficulty, b.raid FROM raids r LEFT JOIN bosses b ON(r.idRaids = b.raid) WHERE replace(replace(r.nameRaid, ' ', ''), '\'', '') like '$rname'";
+
+        $this->data["bosses"] = Db::queryAll($sql);
+
 
         $this->view = "raid";
 
