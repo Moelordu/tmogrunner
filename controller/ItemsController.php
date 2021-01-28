2e0aa1a9f7374009eddee2edf625253068a2c038
Jan Lička <j.licka.st@spseiostrava.cz>
2021-01-28 21:12:55 +0100

    implementování listu itemů

--- controller/ItemsController.php
+++ controller/ItemsController.php
@@ -4,6 +4,8 @@ class ItemsController extends Controller
     public function process($parameters)
     {
 
+        $this->data["items"] = dbget::get("items");
+
         $this->view = "items";
     }
 }
