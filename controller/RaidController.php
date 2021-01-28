b4859e6a6c3319e39751f9c8666e3723360656b7
Jan Lička <j.licka.st@spseiostrava.cz>
2021-01-28 21:03:00 +0100

    raid fix

--- controller/RaidController.php
+++ controller/RaidController.php
@@ -4,8 +4,8 @@ class RaidController extends Controller
     public function process($parameters)
     {
         $rname = text::camelCase($parameters[0]);
-        $sql = "SELECT b.idBosses, b.nameBoss, b.difficulty, b.raid FROM raids r LEFT JOIN bosses b ON(r.idRaids = b.raid) WHERE replace(replace(r.nameRaid, ' ', ''), '\'', '') like '$rname'";
-
+        $sql = "SELECT b.idBosses, b.nameBoss, b.difficulty, b.raid, d.nameDifficulty FROM raids r LEFT JOIN bosses b ON(r.idRaids = b.raid) INNER JOIN difficulty d ON(b.difficulty = d.idDifficulty) WHERE replace(replace(r.nameRaid, ' ', ''), '\'', '') like '$rname'";
+ 
         $this->data["bosses"] = Db::queryAll($sql);
 
 
