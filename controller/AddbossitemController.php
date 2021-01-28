0902caf253c62318ae79ff34546f698e9fe73b91
Jan Lička <j.licka.st@spseiostrava.cz>
2021-01-28 20:45:42 +0100

    fix chyby

--- controller/AddbossitemController.php
+++ controller/AddbossitemController.php
@@ -11,7 +11,7 @@ class AddbossitemController extends Controller
             {
                 $bossitem->newBossItem($_POST);
             }
-            $sql = "SELECT bi.idBosses, b.nameBoss, b.difficulty, d.nameDifficulty, d.pNumber, bi.idItems, i.nameItem FROM bosses b INNER JOIN bosses_has_items bi USING(idBosses) INNER JOIN items i USING(idItems) INNER JOIN difficulty d ON(i.difficulty=d.idDifficulty)";
+            $sql = "SELECT bi.idBosses, b.nameBoss, b.difficulty, d.nameDifficulty, d.pNumber, bi.idItems, i.nameItem FROM bosses b INNER JOIN bosses_has_items bi USING(idBosses) INNER JOIN items i USING(idItems) INNER JOIN difficulty d ON(b.difficulty=d.idDifficulty)";
             $sql1 = "SELECT b.idBosses, b.nameBoss, b.difficulty, d.nameDifficulty, d.pNumber FROM bosses b INNER JOIN difficulty d ON(b.difficulty = d.idDifficulty)";
             $this->data["bosses"] = Db::queryAll($sql1);
             $this->data["items"] = dbget::get("items");
