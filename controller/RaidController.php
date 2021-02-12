7c3e57267a46ec26b7ed81a5061d348199c8ceb6
Jan Lička <j.licka.st@spseiostrava.cz>
2021-02-12 18:06:50 +0100

    největší pepega error co může být
    nejde mi localhost db, nemůžu to otestovat na tom, na serveru mi to nic neřekne, nice

--- controller/RaidController.php
+++ controller/RaidController.php
@@ -6,7 +6,7 @@ class RaidController extends Controller
         $favorite = new favorite();
         $rname = text::camelCase($parameters[0]);
         $sql = "SELECT b.idBosses, b.nameBoss, b.difficulty, b.raid, d.nameDifficulty, u.idUsers
-        FROM raids r LEFT JOIN bosses b ON(r.idRaids = b.raid) INNER JOIN difficulty d ON(b.difficulty = d.idDifficulty) LEFT JOIN users_has_bosses ub ON(b.idBosses = ub.idBosses) LEFT JOIN users u ON(ub.idUsers = u.idUsers)
+        FROM raids r INNER JOIN bosses b ON(r.idRaids = b.raid) INNER JOIN difficulty d ON(b.difficulty = d.idDifficulty) LEFT JOIN users_has_bosses ub ON(b.idBosses = ub.idBosses) LEFT JOIN users u ON(ub.idUsers = u.idUsers)
         WHERE replace(replace(r.nameRaid, ' ', ''), '\'', '') like '$rname'";
  
         $this->data["bosses"] = Db::queryAll($sql);
@@ -16,12 +16,12 @@ class RaidController extends Controller
             {
                 $_POST["idUsers"] = $_SESSION["user"]["idUsers"];
                 $favorite->newFavorite($_POST);
-                $this->redirect("/raid\/" . $parameters[0]);
+                $this->redirect("raid/" . $parameters[0]);
             }
             else
             {
                 $favorite->delFavorite($_POST["idBosses"], $_SESSION["user"]["idUsers"]);
-                $this->redirect("/raid\/" . $parameters[0]);
+                $this->redirect("raid/" . $parameters[0]);
             }
                 
         }
